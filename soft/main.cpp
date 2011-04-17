bce82a995e82cd8fa3d7b90b6f317feb616c25d3
el-bart <>
2011-04-17 14:42:43 +0200

    example usages of existing code.
    modified:
      soft/main.cpp

--- soft/main.cpp
+++ soft/main.cpp
@@ -7,9 +7,12 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>     // <avr/delay.h> once
+#include <avr/sleep.h>
 
 #include "Queue.hpp"
 #include "USART.hpp"
+#include "Timer0.hpp"
+#include "PowerSave.hpp"
 #include "uassert.hpp"
 
 
@@ -25,9 +28,34 @@ ISR(BADISR_vect)
 //
 int main(void)
 {
-  USART::init();
-  DDRB|=0xFF;
+  USART::init();    // prepare USART to work
+  DDRB|=0xFF;       // PB operates as output
+  Timer0 t0;        // configure T0
+  sei();            // allow interrupts globally
 
+#if 0
+  while(1)
+  {
+    PORTB=0xFF;
+    _delay_ms(2);
+    PORTB=0x00;
+    _delay_ms(18);
+  }
+#endif
+
+#if 0
+  while(1)
+  {
+    t0.clearInterruptFlag();
+    PORTB=0xFF;
+    _delay_ms(2);
+    PORTB=0x00;
+    while( !t0.interruptCame() )
+      PowerSave::idle();
+  }
+#endif
+
+#if 0
   while(1)
   {
     for(int i=0; i<2; ++i)
@@ -35,8 +63,11 @@ int main(void)
         _delay_ms(250);
     PORTB^=0xFF;
   }
+#endif
 
   // TODO
+
+  while(1);
   return 0;
 } // main()
 
