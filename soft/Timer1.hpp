ecf57ef269928d20fd0934db92ffa38a79997592
el-bart <>
2011-04-17 15:40:54 +0200

    Timer1 support implemented.
    added:
      soft/Timer1.cpp
      soft/Timer1.hpp

---
+++ soft/Timer1.hpp
@@ -0,0 +1,47 @@
+/*
+ * Timer1.hpp
+ *
+ */
+#ifndef INCLUDE_TIMER1_HPP_FILE
+#define INCLUDE_TIMER1_HPP_FILE
+
+#include "config.hpp"
+#include <avr/io.h>
+#include "Noncopyable.hpp"
+
+/** \brief timer1 wrapper.
+ */
+class Timer1: private Noncopyable
+{
+public:
+  /** \brief initialize T1.
+   *  \note timer starts as stopped.
+   */
+  Timer1(void);
+  /** \brief mark timer as not used.
+   *  \note it does not touch timer settings.
+   */
+  ~Timer1(void);
+
+  /** \brief start timer (resets counter to 0).
+   */
+  void start(void);
+  /** \brief stops counter (resets counter to 0).
+   */
+  void stop(void);
+
+  /** \brief get current counter value.
+   *  \return counter value.
+   *
+   *  this returns number of micro seconds since started. this means
+   *  8000 is 1ms, thus making typical servo to work in range
+   *  8000:16000, with center at 12000, though this may warry between
+   *  different devices.
+   */
+  uint16_t get(void) const
+  {
+    return TCNT1;
+  }
+}; // class Timer1
+
+#endif
