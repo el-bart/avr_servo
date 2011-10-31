d52a2beeb105cce1e5080038675e8d20a136b718
el-bart <>
2011-10-31 19:01:20 +0100

    chrono table updated to generate entries in new format.
    modified:
      src/ChronoTable.cpp

--- src/ChronoTable.cpp
+++ src/ChronoTable.cpp
@@ -3,6 +3,7 @@
 #include <avr/interrupt.h>
 #include "ChronoTable.hpp"
 
+
 ChronoTable::ChronoTable(const Positions defaultPositions)
 {
   // initially start with default values
@@ -11,15 +12,16 @@ ChronoTable::ChronoTable(const Positions defaultPositions)
   update();
 }
 
+
 namespace
 {
-inline void bubbleSort(uint8_t (&idxs)[8], const ChronoTable::Positions &c)
+inline void bubbleSort(uint8_t (&idxs)[SERVO_COUNT], const ChronoTable::Positions &c)
 {
   // find proper order of elements
-  for(uint8_t i=0; i<8; ++i)
+  for(uint8_t i=0; i<SERVO_COUNT; ++i)
   {
     bool changed=false;
-    for(uint8_t j=1; j<8; ++j)
+    for(uint8_t j=1; j<SERVO_COUNT; ++j)
     {
       if( c[idxs[j-1]] > c[idxs[j]] )
       {
@@ -36,36 +38,57 @@ inline void bubbleSort(uint8_t (&idxs)[8], const ChronoTable::Positions &c)
       break;
   }
 } // bubbleSort()
+
+
+void servoNoToMask(const uint8_t servoNo, Entry &out)
+{
+  if(servoNo<6) // PORTB?
+  {
+    const uint8_t tmp=servoNo;
+    out.maskPB_&=~_BV(tmp);
+    return;
+  }
+  if(servoNo<12) // PORTC?
+  {
+    const uint8_t tmp=servoNo-6;
+    out.maskPC_&=~_BV(tmp);
+    return;
+  }
+  if(servoNo<18) // PORTD?
+  {
+    const uint8_t tmp=servoNo-12+2;
+    out.maskPD_&=~_BV(tmp);
+    return;
+  }
+  // if we're here, it means invalid servo number. entry will be skipped...
+} // servoNoToMask()
 } // unnamed namespace
+
 
 void ChronoTable::update(void)
 {
-  cli();                                // disable interrupts for a moment of copying
-  const Positions cur=cur_;             // make local copy (original may change in interrupts)
-  sei();                                // interrupts are welcome now
-  uint8_t idxs[8]={0,1,2,3,4,5,6,7};    // sorted indexes
-  bubbleSort(idxs, cur);                // find proper order of elements
+  uint8_t idxs[SERVO_COUNT]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};  // sorted indexes
+  bubbleSort(idxs, cur_);                                                   // find proper order of elements
 
   uint8_t idxOut=0;
-  for(uint8_t i=0; i<8; ++i)
+  for(uint8_t i=0; i<SERVO_COUNT; ++i)
   {
     // make default mask for this element
-    uint8_t mask=0xFF;
-    mask&=~_BV(idxs[i]);
+    Entry entry={cur_[idxs[i]], 0xFF, 0xFF, 0xFF};
+    servoNoToMask(idxs[i], entry);
     // try packing multiple entries into one, if possible
-    while( i<8-1 && cur[idxs[i]]==cur[idxs[i+1]] )
+    while( i<SERVO_COUNT-1 && cur_[idxs[i]]==cur_[idxs[i+1]] )
     {
       ++i;
-      mask&=~_BV(idxs[i]);
+      servoNoToMask(idxs[i], entry);
     }
     // save this entry
-    const Entry tmp={ cur[idxs[i]], mask };
-    e_[idxOut]=tmp;
+    e_[idxOut]=entry;
     ++idxOut;
   }
 
   // fill up missing elements with zeros
   const Entry zero={0, 0x00, 0x00, 0x00};
-  for(uint8_t i=idxOut; i<8; ++i)
+  for(uint8_t i=idxOut; i<SERVO_COUNT; ++i)
     e_[i]=zero;
 }
