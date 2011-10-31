fd228914a9dbffa6045134ff8a206ab91c13585f
el-bart <>
2011-10-31 20:07:57 +0100

    communication protocol implemented.
    added:
      src/CommProtocol.cpp
      src/CommProtocol.hpp

---
+++ src/CommProtocol.cpp
@@ -0,0 +1,178 @@
+#include "config.hpp"
+#include <ctype.h>
+
+#include "CommProtocol.hpp"
+#include "nameToNumber.hpp"
+
+CommProtocol::CommProtocol(QueueSend &qSend, QueueRecv &qRecv, PersistentSettings &settings):
+  qSend_(qSend),
+  qRecv_(qRecv),
+  settings_(settings)
+{
+}
+
+
+namespace
+{
+uint8_t hex2num(char h)
+{
+  // digit?
+  if('0'<=h && h<='9')
+    return h-'0';
+  // letter?
+  h=tolower(h);
+  if('a'<=h && h<='f')
+    return 10+(h-'a');
+  // error..
+  return 0xFF;
+} // hex2num()
+} // unnamed namespace
+
+
+void CommProtocol::process(ChronoTable::Positions &posTab)
+{
+  while( qRecv_.size()>=1+1+2+1 )
+  {
+    // get servo number
+    const char servoName=qRecv_.pop();
+    if(servoName<'a' || 'r'<servoName)
+    {
+      skipUntilNewCommand();
+      continue;
+    }
+    const uint8_t servoNo=nameToNumber(servoName);
+
+    // get mode
+    const char mode=qRecv_.pop();
+    if(mode!='l' && mode!='h' && mode!='d' && mode!='s')
+    {
+      skipUntilNewCommand();
+      continue;
+    }
+
+    // get position
+    uint8_t posTmp[2];
+    posTmp[0]=hex2num( qRecv_.pop() );
+    if(posTmp[0]>0x0F)
+    {
+      skipUntilNewCommand();
+      continue;
+    }
+    posTmp[1]=hex2num( qRecv_.pop() );
+    if(posTmp[1]>0x0F)
+    {
+      skipUntilNewCommand();
+      continue;
+    }
+    const uint8_t pos=posTmp[0]*16+posTmp[1];
+
+    // check for end of line
+    const char end=qRecv_.pop();
+    if(end!='\n' && end!='\r')
+    {
+      skipUntilNewCommand();
+      continue;
+    }
+
+    // remove extra elements from the queue
+    skipEndMarkers();
+
+    // execute received orders
+    const bool ret=execute(servoNo, mode, pos, posTab);
+    // send confirmation, if it is ok or not
+    qSend_.push(servoName);
+    if(ret)
+    {
+      qSend_.push('o');
+      qSend_.push('k');
+    }
+    else
+    {
+      qSend_.push('E');
+      qSend_.push('R');
+      qSend_.push('R');
+      qSend_.push('O');
+      qSend_.push('R');
+    }
+    qSend_.push('\n');
+  } // while(has_more_date)
+}
+
+
+void CommProtocol::skipUntilNewCommand(void)
+{
+  // remove characters until end of command
+  uint8_t toRemove=0;
+  for(uint8_t i=0; i<qRecv_.size(); ++i)
+  {
+    const char c=qRecv_.peek(i);
+    if(c=='\n' || c=='\r')
+      break;
+    toRemove=i+1;
+  }
+  qRecv_.remove(toRemove);
+
+  // now skip all end markers
+  skipEndMarkers();
+}
+
+
+void CommProtocol::skipEndMarkers(void)
+{
+  uint8_t toRemove=0;
+  for(uint8_t i=0; i<qRecv_.size(); ++i)
+  {
+    const char c=qRecv_.peek(i);
+    if(c!='\n' && c!='\r')
+      break;
+    toRemove=i+1;
+  }
+
+  if(toRemove>0)
+    qRecv_.remove(toRemove);
+}
+
+
+bool CommProtocol::execute(const uint8_t srvNo, const char mode, const uint8_t pos, ChronoTable::Positions &posTab)
+{
+  // read these to make code shorter later on
+  const uint8_t pMin=settings_.posMin().read(srvNo);
+  const uint8_t pMax=settings_.posMax().read(srvNo);
+  uint8_t       tmp =pos;
+  // check mode
+  switch(mode)
+  {
+    case 's':   // set servo to given position
+         if(tmp<pMin)
+           tmp=pMin;
+         else
+           if(tmp>pMax)
+             tmp=pMax;
+         posTab[srvNo]=tmp;
+         return true;
+
+    case 'l':   // set minimum value
+         if(tmp>pMax)
+           tmp=pMax;
+         settings_.posMin().write(srvNo, tmp);
+         return true;
+
+    case 'h':   // set maximum value
+         if(tmp<pMin)
+           tmp=pMin;
+         settings_.posMax().write(srvNo, tmp);
+         return true;
+
+    case 'd':   // set default value
+         if(tmp<pMin)
+           tmp=pMin;
+         else
+           if(tmp>pMax)
+             tmp=pMax;
+         settings_.posDef().write(srvNo, tmp);
+         return true;
+  } // switch(mode)
+
+  // unknown mode...
+  return false;
+}
---
+++ src/CommProtocol.hpp
@@ -0,0 +1,31 @@
+#ifndef INCLUDE_COMMPROTOCOL_HPP_FILE
+#define INCLUDE_COMMPROTOCOL_HPP_FILE
+
+#include "config.hpp"
+#include <inttypes.h>
+
+#include "CommQueues.hpp"
+#include "Noncopyable.hpp"
+#include "ChronoTable.hpp"
+
+class CommProtocol: private Noncopyable
+{
+public:
+  CommProtocol(QueueSend &qSend, QueueRecv &qRecv, PersistentSettings &settings);
+
+  /** \brief updates basing on protocol data.
+   *  \param posTab positions to update.
+   */
+  void process(ChronoTable::Positions &posTab);
+
+private:
+  void skipUntilNewCommand(void);
+  void skipEndMarkers(void);
+  bool execute(uint8_t srvNo, char mode, uint8_t pos, ChronoTable::Positions &posTab);
+
+  QueueSend          &qSend_;
+  QueueRecv          &qRecv_;
+  PersistentSettings &settings_;
+}; // class CommProtocol
+
+#endif
