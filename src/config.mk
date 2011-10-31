b7a1fe19984f0c5d748eee434ee10139657da4e7
el-bart <>
2011-10-31 16:12:56 +0100

    build updated to reflect AVR-onyl building.
    modified:
      bzr_build.conf
      src/config.mk

--- src/config.mk
+++ src/config.mk
@@ -1,18 +1,11 @@
 # name of the application
 export PROGRAM_NAME:=avr_servo
 
-# default
-TC:=avr
-
-#
-# AVR
-#
-ifeq ($(TC),avr)
 # toolchain
 export CC :=avr-gcc
 export CXX:=avr-g++
 export OBJCOPY:=avr-objcopy
-export LD :=$(CC)
+export LD :=$(CXX)
 
 # tools settings
 export MCU_TYPE:=atmega8
@@ -22,25 +15,3 @@ export MCU_PROG:=m8
 export CFLAGS  +=-mmcu=$(MCU_TYPE)
 export CXXFLAGS+=-mmcu=$(MCU_TYPE)
 export LDFLAGS +=-mmcu=$(MCU_TYPE)
-endif
-
-#
-# GCC
-#
-ifeq ($(TC),gcc)
-# toolchain
-export CC :=gcc
-export CXX:=g++
-export OBJCOPY:=objcopy
-export LD :=$(CC)
-endif # TC
-
-#
-# common
-#
-
-# extra flags, if needed
-TMP:=-I$(CURDIR)/backend/$(TC)
-export CFLAGS  +=$(TMP)
-export CXXFLAGS+=$(TMP)
-export LDFLAGS +=
