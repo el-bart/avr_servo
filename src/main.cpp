a9953a857d1b83b9d5c96c85bfe122a1e83d3aec
el-bart <>
2011-11-10 17:13:12 +0100

    added full, 20ms cycle. removed dead code from main;
    modified:
      src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -46,32 +46,62 @@ inline Positions getDefaultPositions(const PersistentSettings &s)
 int main(void)
 {
 #if 0
-  #define USART_UBRR(baud,f) ( ((f)/((baud)*16L)) -1 )
-  DDRB|=0xFF;       // PB operates as output
+  PersistentSettings settings;                                      // persistent settings
+  ChronoTable        chronoTable( getDefaultPositions(settings) );  // chronology table
+  QueueSend          qSend;                                         // output queue
+  QueueRecv          qRecv;                                         // input queue
+  USART              usart(qSend, qRecv);                           // prepare USART to work
+  Timer1             t1;                                            // configure T1
+  Timer2             t2;                                            // configure T2
+  PhaseGenerator     phaseGen;                                      // controling facility
+  CommProtocol       proto(qSend, qRecv, settings);                 // protocol parser
+  sei();                                                            // allow interrupts globally
 
-  // clock divider register (computed from baud rate and oscilator frequency)
-  UBRRH=(uint8_t)( (USART_UBRR(USART_BAUD, F_CPU)>>8) & 0x00FF );
-  UBRRL=(uint8_t)( (USART_UBRR(USART_BAUD, F_CPU)>>0) & 0x00FF );
+  /*
+  for(int i=0; i<SERVO_COUNT; ++i)
+    chronoTable.currentPos()[i]=256/2;
+    */
 
-  // enable transciever
-  UCSRB|= _BV(RXEN);    // RX enable
-  UCSRB|= _BV(TXEN);    // TX enable
-
-  // configure proper pins as in (RX) and out (TX)
-  DDRD &=~_BV(PD0);     // RX as in
-  PORTD|= _BV(PD0);     // RX high with pull-up
-  DDRD |= _BV(PD1);     // TX as out
-
+  uint8_t lastStep=0xFF;
   while(true)
   {
-    // read
-    while( !(UCSRA&(1<<RXC)) );
-    const uint8_t c=UDR;
-    UCSRA&=~(1<<RXC);
-    // send
-    UDR=c+1;
-    while( !(UCSRA&(1<<UDRE)) );
-  }
+
+    // wait in idle mode while nothing happens
+    const uint8_t step=t2.currentStep();
+    if(step==lastStep)
+    {
+      PowerSave::idle();
+      continue;
+    }
+    lastStep=step;
+
+    // something has to be done - process next stage
+    switch(step)
+    {
+      // initial 'high' state
+      case 0:
+           phaseGen.rise();
+           chronoTable.update();
+           break;
+
+      // PWM generation for servos
+      case 1:
+           phaseGen.generate( t1, chronoTable.currentEntries() );
+           break;
+
+      // 'wait' in 'low' state untile the end of the cycle
+      default:
+           if(step<20)
+           {
+             proto.process( chronoTable.currentPos() );
+             usart.sendData();
+             break;
+           }
+           t2.resetStep();      // reset step counter (i.e. start new cycle)
+           break;
+    } // switch(current_step)
+
+  } // while(true)
 #endif
 
   Watchdog           wdg;                                           // initialize watchdog (disable by default)
@@ -115,103 +145,20 @@ int main(void)
            phaseGen.fall();
            break;
 
-      // 'wait' in 'low' state
-      case 2:
-      case 3:
-      case 4:
-           proto.process( chronoTable.currentPos() );
-           usart.sendData();
-           break;
-
-      case 5:
+      // 'wait' in 'low' state untile cycle ends
       default:
+           if(step<20)
+           {
+             proto.process( chronoTable.currentPos() );
+             usart.sendData();
+             break;
+           }
            wdg.reset();         // signal watchdog system is working fine
            t2.resetStep();      // reset step counter (i.e. start new cycle)
            break;
     } // switch(current_step)
 
   } // while(true)
-
-#if 0
-  while(true)
-  {
-    USART::send('0'+bytes_);
-    _delay_ms(250);
-    USART::send(' ');
-    _delay_ms(250);
-  }
-#endif
-
-#if 0
-  while(true)
-  {
-    const uint8_t b=USART::recv();
-    /*
-    USART::send('\n');
-    USART::send(b-1);
-    USART::send(b+1);
-    USART::send('\n');
-    */
-    //USART::send('0'+bytes_);
-    USART::send(b);
-    _delay_ms(250);
-    //USART::send(' ');
-    _delay_ms(250);
-  }
-#endif
-
-#if 0
-  while(true)
-  {
-    for(char c='a'; c<='z'; ++c)
-    {
-      USART::send(c);
-      _delay_ms(100);
-    }
-    USART::send('\n');
-    _delay_ms(100);
-  }
-#endif
-
-#if 0
-  cli();
-  while(true)
-  {
-    for(char c='a'; c<='z'; ++c)
-    {
-      UDR=c;
-      _delay_ms(100);
-    }
-    UDR='\n';
-    _delay_ms(100);
-  }
-#endif
-
-#if 0
-  while(1)
-  {
-    // initial preparations
-    t0.clearInterruptFlag();
-    t1.start();
-    // start cycle
-    PORTB=0xFF;
-    // end cycles high-cycles for servos in proper order
-    for(uint8_t i=0; i<8; ++i)
-    {
-      const ChronoTable::Entry e=ct.get(i); // get copy for faster access
-      while( t1.get()<e.time_ );            // wait until to be triggered
-      PORTB&=e.mask_;                       // apply mask, as requested
-    }
-    // end high-value
-    PORTB=0x00;
-    t1.stop();
-    // now wait until the end of the period
-    while( !t0.interruptCame() )
-      PowerSave::idle();
-    // compute new servo positions table
-    ct.compute();
-  }
-#endif
 
   // code never reaches here
   return 0;
