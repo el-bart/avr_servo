245c3a2f3a09199016bbe43cb133cf1829929a24
el-bart <>
2011-11-10 10:16:53 +0100

    fuse bits moved to config file; fuse bits configured for high res. ext. oscilator with startup time 258ck+4ms
    modified:
      src/Makefile
      src/config.mk

--- src/config.mk
+++ src/config.mk
@@ -8,8 +8,12 @@ export OBJCOPY:=avr-objcopy
 export LD :=$(CXX)
 
 # tools settings
-export MCU_TYPE:=atmega8
-export MCU_PROG:=m8
+export MCU_TYPE :=atmega8
+export MCU_PROG :=m8
+export LOW_FUSE :=0xce
+export HIGH_FUSE:=0xd9
+#export LOW_FUSE :=0xe1
+#export HIGH_FUSE:=0xd9
 
 # extra flags, if needed
 export CFLAGS  +=-mmcu=$(MCU_TYPE)
