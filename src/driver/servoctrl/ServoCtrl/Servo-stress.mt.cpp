6265a1e8442b7afcee17d059595c295bc15c6277
el-bart <>
2011-11-11 22:52:58 +0100

    command line upgrades.
    modified:
      src/driver/servoctrl/ServoCtrl/Servo-stress.mt.cpp

--- src/driver/servoctrl/ServoCtrl/Servo-stress.mt.cpp
+++ src/driver/servoctrl/ServoCtrl/Servo-stress.mt.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <boost/lexical_cast.hpp>
 
 #include "ServoCtrl/Servo.hpp"
 
@@ -7,20 +8,22 @@ using namespace ServoCtrl;
 
 int main(int argc, char **argv)
 {
-  if(argc!=1+2)
+  if(argc!=1+4)
   {
-    cerr<<argv[0]<<" <dev> <servo>"<<endl;
+    cerr<<argv[0]<<" <dev> <servo> <delay-ms> <fast[01]>"<<endl;
     return 1;
   }
+  const unsigned int delay=boost::lexical_cast<unsigned int>(argv[3]);
+  const bool         fast =boost::lexical_cast<bool>(argv[4]);
 
   CommDevicePtrNN dev{ new CommDevice{argv[1]} };
-  Servo           servo{ ServoName{argv[2][0]}, dev, false };
+  Servo           servo{ ServoName{argv[2][0]}, dev, fast };
 
   uint8_t pos=50;
   uint8_t dp =1;
   while(true)
   {
-    usleep(10*1000);
+    usleep(delay*1000);
     cout<<"sending pos "<<int{pos}<<"..."<<endl;
     try
     {
