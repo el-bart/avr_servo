9b4579b7143fa467b6c398a63494cfa8aad6be75
el-bart <>
2011-11-11 21:56:24 +0100

    servo moving loop is done.
    modified:
      src/driver/pantiltapp/PanTiltApp/NavWin.cpp
      src/driver/pantiltapp/PanTiltApp/NavWin.hpp
      src/driver/pantiltapp/PanTiltApp/main.cpp

--- src/driver/pantiltapp/PanTiltApp/NavWin.cpp
+++ src/driver/pantiltapp/PanTiltApp/NavWin.cpp
@@ -1,11 +1,118 @@
 #include <iostream>
+#include <mutex>
 
 #include "NavWin.hpp"
 
 using namespace std;
 
 
-NavWin::NavWin(void)
+struct NavWin::CommThread
+{
+  typedef std::unique_lock<std::mutex> Lock;
+
+  CommThread(ServoCtrl::CommDevicePtrNN dev, ServoCtrl::ServoName nameX, ServoCtrl::ServoName nameY):
+    posXsent_{0},
+    posYsent_{0},
+    srvX_{nameX, dev},
+    srvY_{nameY, dev},
+    quit_{false}
+  {
+    setX(127);
+    setY(127);
+  }
+
+  void quit(void)
+  {
+    Lock lock{m_};
+    quit_=true;
+  }
+
+  void setX(uint8_t x)
+  {
+    Lock lock{m_};
+    posX_=x;
+  }
+  uint8_t getX(void) const
+  {
+    Lock lock{m_};
+    return posX_;
+  }
+
+  void setY(uint8_t y)
+  {
+    Lock lock{m_};
+    posY_=y;
+  }
+  uint8_t getY(void) const
+  {
+    Lock lock{m_};
+    return posY_;
+  }
+
+  void run(void)
+  {
+    while(!quitNow())
+    {
+      if( posXsent_!=getX() )
+      {
+        const uint8_t p=getX();
+        send(srvX_, p);
+        posXsent_=p;
+      }
+
+      if( posYsent_!=getY() )
+      {
+        const uint8_t p=getY();
+        send(srvY_, p);
+        posYsent_=p;
+      }
+
+      // TODO: make this conditional
+      while(posXsent_==getX() && posYsent_==getY() && !quitNow())
+        std::this_thread::yield();
+    }
+  }
+
+private:
+  bool quitNow(void) const
+  {
+    Lock lock{m_};
+    return quit_;
+  }
+
+  void send(ServoCtrl::Servo &srv, const uint8_t pos)
+  {
+    cout<<"sedning "<<int{pos}<<endl;
+    try
+    {
+      srv.setPos(pos);
+    }
+    catch(const std::exception &ex)
+    {
+      cerr<<"oops: "<<ex.what()<<" - proceeding"<<endl;
+    }
+  }
+
+  mutable std::mutex m_;
+
+  ServoCtrl::Servo srvX_;
+  ServoCtrl::Servo srvY_;
+
+  uint8_t posXsent_;
+  uint8_t posYsent_;
+
+  uint8_t posX_;
+  uint8_t posY_;
+
+  bool quit_;
+}; // struct NavWin::CommThread
+
+
+
+NavWin::NavWin(ServoCtrl::CommDevicePtrNN dev, ServoCtrl::ServoName nameX, ServoCtrl::ServoName nameY):
+  pressed_{false},
+  thImpl_{ new CommThread{dev, nameX, nameY} },
+  th_{ [&thImpl_]{ thImpl_->run(); } }
 {
   set_title("mouse position event");
   add_events( Gdk::POINTER_MOTION_MASK | Gdk::POINTER_MOTION_HINT_MASK | Gdk::BUTTON_PRESS_MASK | Gdk::BUTTON_RELEASE_MASK );
@@ -15,25 +122,77 @@ NavWin::NavWin(void)
 NavWin::~NavWin(void)
 {
   cout<<"exit..."<<endl;
+  thImpl_->quit();
+  thImpl_->setX(166);
+  thImpl_->setY(166);
+  if(th_.joinable())
+    th_.join();
 }
+
 
 bool NavWin::on_motion_notify_event(GdkEventMotion* event)
 {
-  if(event!=NULL)
-    cout<<"mouse pos: "<<event->x<<" "<<event->y<<endl;
+  if(event==NULL)
+    return false;
+
+  //cout<<"mouse pos: "<<event->x<<" "<<event->y<<endl;
+  if(!pressed_)
+    return true;
+
+  posUpdate(posX_, lastX_, event->x);
+  thImpl_->setX(posX_);
+  lastX_=event->x;
+
+  posUpdate(posY_, lastY_, event->y);
+  thImpl_->setY(posY_);
+  lastY_=event->y;
+
   return true;
 }
 
 bool NavWin::on_button_press_event(GdkEventButton* event)
 {
-  if(event!=NULL)
-    cout<<"press: "<<event->x<<" "<<event->y<<endl;
+  if(event==NULL)
+    return false;
+
+  //cout<<"press: "<<event->x<<" "<<event->y<<endl;
+  pressed_=true;
+  lastX_  =event->x;
+  lastY_  =event->y;
   return true;
 }
 
+
 bool NavWin::on_button_release_event(GdkEventButton* event)
 {
-  if(event!=NULL)
-    cout<<"release: "<<event->x<<" "<<event->y<<endl;
+  if(event==NULL)
+    return false;
+
+  //cout<<"release: "<<event->x<<" "<<event->y<<endl;
+  pressed_=false;
   return true;
+}
+
+
+void NavWin::posUpdate(uint8_t &pos, double last, double now)
+{
+  const int dp=posDiff(last, now);
+  int       p =pos+dp;
+  // limit it a bit...
+  if(p<50)
+    p=50;
+  if(p>200)
+    p=200;
+  // ok
+  pos=p;
+}
+
+
+int NavWin::posDiff(double last, double now)
+{
+  if(last<now)
+    return +1;
+  if(last>now)
+    return -1;
+  return 0;
 }
--- src/driver/pantiltapp/PanTiltApp/NavWin.hpp
+++ src/driver/pantiltapp/PanTiltApp/NavWin.hpp
@@ -1,18 +1,41 @@
 #ifndef HELLOWORLDWINDOW_HPP_FILE
 #define HELLOWORLDWINDOW_HPP_FILE
 
+#include <thread>
+#include <memory>
+#include <cinttypes>
 #include <gtkmm/window.h>
+
+#include "ServoCtrl/CommDevice.hpp"
+#include "ServoCtrl/ServoName.hpp"
+#include "ServoCtrl/Servo.hpp"
 
 class NavWin: public Gtk::Window
 {
 public:
-  NavWin(void);
+  NavWin(ServoCtrl::CommDevicePtrNN dev, ServoCtrl::ServoName nameX, ServoCtrl::ServoName nameY);
   ~NavWin(void);
 
 protected:
+  struct CommThread;
+
   virtual bool on_motion_notify_event(GdkEventMotion* event);
   virtual bool on_button_press_event(GdkEventButton* event);
   virtual bool on_button_release_event(GdkEventButton* event);
+
+  void posUpdate(uint8_t &pos, double last, double now);
+  int posDiff(double last, double now);
+  void send(ServoCtrl::Servo &srv, uint8_t pos);
+
+  bool    pressed_;
+  double  lastX_;
+  double  lastY_;
+
+  uint8_t posX_;
+  uint8_t posY_;
+
+  std::unique_ptr<CommThread> thImpl_;
+  std::thread                 th_;
 };
 
 #endif
