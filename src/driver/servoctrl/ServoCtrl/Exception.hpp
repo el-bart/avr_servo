2abed9fb67ea7dcbda6b6203d170a5830b694190
el-bart <>
2011-11-11 14:08:23 +0100

    servo name and exception class created.
    added:
      src/driver/servoctrl/ServoCtrl/Exception.hpp
      src/driver/servoctrl/ServoCtrl/ServoName.hpp
      src/driver/servoctrl/ServoCtrl/ServoName.t.cpp

---
+++ src/driver/servoctrl/ServoCtrl/Exception.hpp
@@ -0,0 +1,40 @@
+#ifndef INCLUDE_SERVOCTRL_EXCEPTION_HPP_FILE
+#define INCLUDE_SERVOCTRL_EXCEPTION_HPP_FILE
+
+#include <string>
+#include <stdexcept>
+
+namespace ServoCtrl
+{
+
+struct Exception: public std::runtime_error
+{
+  explicit Exception(std::string message):
+    std::runtime_error{message}
+  {
+  }
+
+protected:
+  struct Strm
+  {
+  public:
+    template<typename T>
+    Strm &operator<<(const T &t)
+    {
+      ss_<<t;
+      return *this;
+    }
+
+    operator std::string(void) const
+    {
+      return ss_.str();
+    }
+
+  private:
+    std::stringstream ss_;
+  }; // struct Strm
+}; // struct Exception
+
+} // namespace ServoCtrl
+
+#endif
---
+++ src/driver/servoctrl/ServoCtrl/ServoName.hpp
@@ -0,0 +1,54 @@
+#ifndef INCLUDE_SERVOCTRL_SERVONAME_HPP_FILE
+#define INCLUDE_SERVOCTRL_SERVONAME_HPP_FILE
+
+#include "ServoCtrl/Exception.hpp"
+
+
+namespace ServoCtrl
+{
+
+class ServoName
+{
+public:
+  struct ExceptionInvalidServo: public Exception
+  {
+    explicit ExceptionInvalidServo(const char name):
+      Exception{ Strm{}<<"invalid servo name '"<<name<<"'" }
+    {
+    }
+  }; // struct ExceptionInvalidServo
+
+
+  explicit ServoName(const char name):
+    name_{ check(name) }
+  {
+  }
+
+  char getName(void) const
+  {
+    return name_;
+  }
+
+  static constexpr char first(void)
+  {
+    return 'a';
+  }
+  static constexpr char last(void)
+  {
+    return 'r';
+  }
+
+private:
+  char check(const char name)
+  {
+    if(name<first() || last()<name)
+      throw ExceptionInvalidServo(name);
+    return name;
+  }
+
+  char name_;
+}; // class ServoName
+
+} // namespace ServoCtrl
+
+#endif
