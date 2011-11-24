e64c3d306a529be36cfda9186621cb297a836bbf
el-bart <>
2011-11-24 15:18:57 +0100

    dead code removeal.
    modified:
      src/avr/Timer1.cpp

--- src/avr/Timer1.cpp
+++ src/avr/Timer1.cpp
@@ -7,21 +7,6 @@
 
 Timer1::Timer1(void)
 {
-  // set timer 1
-  //TCCR1A=0x00;          // all features disable, operate in normal mode
-
-  /*
-  TCCR1A&=~_BV(COM1A1); // disconnect OC1A
-  TCCR1A&=~_BV(COM1A0); // ...
-  TCCR1A&=~_BV(COM1B1); // disconnect OC1B
-  TCCR1A&=~_BV(COM1B0); // ...
-  TCCR1B&=~_BV(WGM13);  // normal opration mode
-  TCCR1B&=~_BV(WGM12);  // ...
-  TCCR1A&=~_BV(WGM11);  // ...
-  TCCR1A&=~_BV(WGM10);  // ...
-  //TCCR1C&=~_BV(FOC1A);  // disable force output compare
-  //TCCR1C&=~_BV(FOC1B);  // disable force output compare
-  */
   // keep stopped by default
   stop();
 }
