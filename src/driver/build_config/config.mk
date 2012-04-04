4902b6fab75dd1214de7fa52cada7576f7bb9118
Bartek Szurgot <bartek.szurgot@baszerr.org>
2012-04-04 19:15:42 +0200

    gcc-4.7 is now supported along with 'default' gcc

--- src/driver/build_config/config.mk
+++ src/driver/build_config/config.mk
@@ -21,8 +21,13 @@ export WITH_CCACHE:=1
 # default features set
 export FEATURES:=
 
-# project-specific flags
+# gcc-4.7 is the first one to use C++11 standard
+ifeq ($(TC),gcc47)
 TMP:=-std=c++11
+else
+TMP:=-std=c++0x
+endif
+# project-specific flags
 export USER_OPT_FLAGS:=$(TMP)
 export USER_DBG_FLAGS:=$(TMP)
 export USER_PRF_FLAGS:=$(TMP)
